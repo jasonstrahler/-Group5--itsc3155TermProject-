e3901fa7fe16e67b3c36e9f2a7c5698476d28313
David Johnson <djohn270@uncc.edu>
2019-03-19 21:05:16 +0000

    Set the root view to Login

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'classroom/classroom'
   get 'student/student'
   get 'login/login'
+  
+  root 'login#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
