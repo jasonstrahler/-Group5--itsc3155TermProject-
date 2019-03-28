5247d4f2ddef982c5c94d55680fc4f88e8e349ab
Anthony DeCenzo <adecenzo@uncc.edu>
2019-03-28 09:40:53 +0000

    Changed root route to login page and removed the navbar for users not signed in

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   resources :classrooms
   resources :students
   
-  root 'login#login'
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
