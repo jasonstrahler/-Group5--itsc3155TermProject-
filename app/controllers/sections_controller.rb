4481b4d12bea8eaf0af42ba8441feec53e4e1246
David Johnson <djohn270@uncc.edu>
2019-04-20 19:22:53 +0000

    Added Assignments table to section view.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -5,6 +5,7 @@ class SectionsController < ApplicationController
         @students = Student.where("section_id = ?", params[:id])
         
         @currentUser = User.find(current_user.id)
+        @assignment = Assignment.where("section_id = ?", params[:id])
     end
     
     def new
