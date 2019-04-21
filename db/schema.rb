68774e55f57443d2ac8348e7e1cf2b6f5cf01198
Jason Strahler <jstrahl1@uncc.edu>
2019-04-21 18:02:34 +0000

    Styled the sections view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_04_20_033229) do
-#USER ID IN ASSIGNMENTS REFERS TO STUDENTS
+
   create_table "assignments", force: :cascade do |t|
     t.string "title"
     t.string "description"
